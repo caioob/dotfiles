195a281b428978eaec3257d60d749c2f67b90941
caioob <ritiosbr@protonmail.com>
2023-08-18 13:03:07 -0300

    Added chromaterm config

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
