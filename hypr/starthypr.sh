9a608c8a56e070856c505f01ccae594dc3b7372f
caioob <ritiosbr@protonmail.com>
2024-03-28 19:21:55 -0300

    hypr

---
+++ hypr/starthypr.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Wallpaper
+swww init &
+
+# walpaper
+swww img $HOME/Downloads/Matoi-Ryuuko-Blend-S-anime-anime-girls-red-Kill-la-Kill-1827471-wallhere.com.jpg &
+
+# NetworkManager applet
+nm-applet --indicator &
+
+# Waybar
+waybar &
+
+# Notification manager
+dunst &
+
+discord
