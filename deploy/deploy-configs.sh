555fc982dfabcf3c3538c92fdf8efb0916122717
caioob <ritiosbr@protonmail.com>
2024-05-03 13:17:01 -0300

    Fixed linking issues

--- deploy/deploy-configs.sh
+++ deploy/deploy-configs.sh
@@ -3,15 +3,22 @@ CONFIGDIR="$HOME/.config"
 if [ -z $1 ]
 then
 	echo "Missing parameters, please send your platform ['note', 'desktop']"
-	rm "$CONFIGDIR/hypr/monitors.conf"
 	cp -rf ../.config/* "$CONFIGDIR"
 else
 	if [ $1 == "note" ]
 	then
+		echo "Removing old monitors.conf link if existent"
+		rm -f "$CONFIGDIR/hypr/monitors.conf" && echo "monitors.conf link successfulky removed"
+		echo "Creating new link"
 		ln -s "$CONFIGDIR/hypr/monitors_note.conf" "$CONFIGDIR/hypr/monitors.conf"
+		echo "New link created"
 	elif [ $1 == "desktop" ]
 	then
+		echo "Removing old monitors.conf link if existent"
+		rm -f "$CONFIGDIR/hypr/monitors.conf" && echo "monitors.conf link successfulky removed"
+		echo "Creating new link"
 		ln -s "$CONFIGDIR/hypr/monitors_note.conf" "$CONFIGDIR/hypr/monitors.conf"
+		echo "New link created"
 	fi
 fi
 
