eaaf6c424a34403578d4c22ac7bed96321d3ff3c
caioob <ritiosbr@protonmail.com>
2024-05-03 14:00:58 -0300

    added input config

--- deploy/deploy-configs.sh
+++ deploy/deploy-configs.sh
@@ -1,4 +1,16 @@
 CONFIGDIR="$HOME/.config"
+PLATFORM=$1
+
+
+linking () {
+	linkingType="$1"
+	linkingPlatform="$2"
+	echo "Removing old $linkingType.conf link if existent"
+	rm -f "$CONFIGDIR/hypr/$linkingType.conf" && echo "Links successfully removed"
+	echo "Creating new links"
+	ln -s "$CONFIGDIR/hypr/$linkingType""_""$linkingPlatform.conf" "$CONFIGDIR/hypr/$linkingType.conf"
+	echo "New $linkingType.conf created"
+}
 
 if [ -z $1 ]
 then
@@ -8,20 +20,14 @@ else
 	then
 		cp -rf ../.config/* "$CONFIGDIR"
 		cp -rf ../.config/wallpapers "$CONFIGDIR"
-		echo "Removing old monitors.conf link if existent"
-		rm -f "$CONFIGDIR/hypr/monitors.conf" && echo "monitors.conf link successfulky removed"
-		echo "Creating new link"
-		ln -s "$CONFIGDIR/hypr/monitors_note.conf" "$CONFIGDIR/hypr/monitors.conf"
-		echo "New link created"
+		linking "monitors" $PLATFORM
+		linking "input" $PLATFORM
 	elif [ $1 == "desktop" ]
 	then
 		cp -rf ../.config/* "$CONFIGDIR"
 		cp -rf ../.config/wallpapers "$CONFIGDIR"
-		echo "Removing old monitors.conf link if existent"
-		rm -f "$CONFIGDIR/hypr/monitors.conf" && echo "monitors.conf link successfulky removed"
-		echo "Creating new link"
-		ln -s "$CONFIGDIR/hypr/monitors_note.conf" "$CONFIGDIR/hypr/monitors.conf"
-		echo "New link created"
+		linking "monitors" $PLATFORM
+		linking "input" $PLATFORM
 	fi
 fi
 
