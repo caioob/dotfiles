fda070be214b035e5bad9cd35cb6d221cc93f6ba
caioob <ritiosbr@protonmail.com>
2024-05-03 13:40:19 -0300

    fixed deploy script

--- deploy/deploy-configs.sh
+++ deploy/deploy-configs.sh
@@ -3,10 +3,11 @@ CONFIGDIR="$HOME/.config"
 if [ -z $1 ]
 then
 	echo "Missing parameters, please send your platform ['note', 'desktop']"
-	cp -rf ../.config/* "$CONFIGDIR"
 else
 	if [ $1 == "note" ]
 	then
+		cp -rf ../.config/* "$CONFIGDIR"
+		cp -rf ../.config/wallpapers "$CONFIGDIR"
 		echo "Removing old monitors.conf link if existent"
 		rm -f "$CONFIGDIR/hypr/monitors.conf" && echo "monitors.conf link successfulky removed"
 		echo "Creating new link"
@@ -14,6 +15,8 @@ else
 		echo "New link created"
 	elif [ $1 == "desktop" ]
 	then
+		cp -rf ../.config/* "$CONFIGDIR"
+		cp -rf ../.config/wallpapers "$CONFIGDIR"
 		echo "Removing old monitors.conf link if existent"
 		rm -f "$CONFIGDIR/hypr/monitors.conf" && echo "monitors.conf link successfulky removed"
 		echo "Creating new link"
